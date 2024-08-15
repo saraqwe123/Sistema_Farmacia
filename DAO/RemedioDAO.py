5573f91dab8de8eb252f4dce507f53a651ae195d
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-08-15 11:59:08 -0300

    Update RemedioDAO.py

--- DAO/RemedioDAO.py
+++ DAO/RemedioDAO.py
@@ -15,12 +15,11 @@ class RemedioDAO:
 
 
     def gravarBD(self, linha):
-        nomeRemedio = input("Qual é o nome do remédio?: ")
-        ean13 = input("Qual é o ean13 do remédio?: ")
-        ncm = int(input("Qual é o ncm do remédio?: "))
-        precoVenda = float(input("Qual é o preço do remédio?: "))
-        fabricante = input("Qual é o fabricante do remédio?: ")
-        substancia = input("Qual é a substância do remédio?: ")
-        registroMS = input("Qual é o registro MS do remédio?: ")
-        #sql = "insert INTO Remedio (registroMS, nomeRemedio, ean13, ncm, precoVenda, fabricante, substancia) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)", registroMS, nomeRemedio, ean13, ncm, precoVenda, fabricante, substancia
-        return remedio.Remedio(registroMS, nomeRemedio, ean13, ncm, precoVenda, fabricante, substancia)
+        rem = remedio.Remedio()
+        rem.nomeRemedio = linha["nome"]
+        rem.ean13 = linha["ean13"]
+        rem.fabricante = linha["fabricante"]
+        rem.ncm = linha["ncm"]
+        rem.precoVenda = linha["preço de venda"]
+        rem.registroMS = linha["registro MS"]
+        rem.substancia = linha["substancia"]
